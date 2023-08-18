7c9d06e3ac4ebf8240428009a399409c4a8e2eff
TinyuChiu <TInyu.Zhao@gmail.com>
2023-08-18 20:44:41 +0800

    standardized naming

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
-# MODULE DIRECT STEPMOTOR
+# M5Module-Stepmotor
 
 ## Overview
 
-**MODULE DIRECT STEPMOTOR** is adapted to M5 core series, the HR8825 chip solution provides a 3-way bipolar stepper motor control interface. You can implement independent control or multi-axis motor linkage after this module is stacked with M5 core series of the internal ESP32 generates signals directly to the driver chip.
+### SKU:M039
+
+**M5Module-Stepmotor** is adapted to M5 core series, the HR8825 chip solution provides a 3-way bipolar stepper motor control interface. You can implement independent control or multi-axis motor linkage after this module is stacked with M5 core series of the internal ESP32 generates signals directly to the driver chip.
 
 This module integrated TCA9554 IO expansion chip provides 4 sets of input signal terminals + 3 sets of stepper motor subdivision control + 1 set of drive chip enable control, through the I2C interface control, can monitor and control the 8 expansion IO state, as well as for external limit switch, dynamic subdivision adjustment, motor braking. 
 
 ## Related Link
 
-[Document](https://docs.m5stack.com/en/module/stepmotor_driver)
+[M5Module-Stepmotor - Document & Datasheet](https://docs.m5stack.com/en/module/stepmotor_driver)
 
 ## License
 
-[MODULE DIRECT STEPMOTOR - MIT](LICENSE)
+[M5Module-Stepmotor - MIT](LICENSE)
--- example/StepmotorPulse_M039/StepmotorPulse_M039.ino
+++ example/StepmotorPulse_M039/StepmotorPulse_M039.ino
@@ -3,28 +3,27 @@
 * Copyright (c) 2021 by M5Stack
 *                  Equipped with M5Core sample source code
 *                          配套  M5Core 示例源代码
-* Visit the website for more
+* Visit for more
 information：https://docs.m5stack.com/en/module/stepmotor_driver
 * 获取更多资料请访问：https://docs.m5stack.com/zh_CN/module/stepmotor_driver
 *
-* describe: Stepmotor With Pluse.
-* date：2021/11/14
+* Describe: Stepmotor With Pluse.
+* Date: 2021/11/14
 *******************************************************************************
   StepMotor Driver Module TEST Example,Directly drive the motor using pulse
 signals. 步进电机模块测试示例,使用脉冲信号直接驱动电机
 */
 #include "M5Stack.h"
-#include "MODULE_DIRECT_STEPMOTOR.h"
+#include "Module_Stepmotor.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 // #define VERSION_1_0
 
-String inputString = "";
-bool stringComplete = false; 
+String inputString  = "";
+bool stringComplete = false;
 
 static DIRECT_STEPMOTOR driver;
-
 
 void setup() {
     M5.begin(true, false, true, false);
@@ -32,7 +31,7 @@ void setup() {
     M5.Lcd.setTextSize(2);
     M5.Lcd.setTextColor(TFT_GREEN);
     M5.Lcd.setTextDatum(MC_DATUM);
-    M5.Lcd.drawString("DIRECT_STEPMOTOR", 160, 10,2);
+    M5.Lcd.drawString("DIRECT_STEPMOTOR", 160, 10, 2);
     M5.Lcd.setTextColor(TFT_YELLOW);
     M5.Lcd.drawString("LIMIT IO STATUS", 160, 90, 2);
     M5.Lcd.drawString("DIR", 70, 220, 2);
@@ -48,9 +47,9 @@ void setup() {
     driver.init(Wire);
 
 #ifndef VERSION_1_0
-    driver.resetMotor(0,0);
-    driver.resetMotor(1,0);
-    driver.resetMotor(2,0);
+    driver.resetMotor(0, 0);
+    driver.resetMotor(1, 0);
+    driver.resetMotor(2, 0);
 #else
     driver.setMicrostepResolution(DIRECT_STEPMOTOR::kMicrosteps16);
 #endif
@@ -76,23 +75,20 @@ void setup() {
 }
 
 void loop() {
-    static uint8_t step_dir = 1;
+    static uint8_t step_dir  = 1;
     static uint8_t reset_mtr = 0;
     static DIRECT_STEPMOTOR::MicrostepResolution_t micro_res =
         DIRECT_STEPMOTOR::kMicrosteps16;
     Serial1.print("Y");
     delay(25);
     M5.update();
-    if(Serial2.available())
-    {
-      char inChar = (char)Serial2.read();
-      if (inChar == 'Y') {
-        M5.Lcd.fillRect(105, 180, 120, 20, TFT_GREEN);
-      }
-    }
-    else
-    {
-      M5.Lcd.fillRect(105, 180, 120, 20, TFT_RED);
+    if (Serial2.available()) {
+        char inChar = (char)Serial2.read();
+        if (inChar == 'Y') {
+            M5.Lcd.fillRect(105, 180, 120, 20, TFT_GREEN);
+        }
+    } else {
+        M5.Lcd.fillRect(105, 180, 120, 20, TFT_RED);
     }
     if (M5.BtnA.wasPressed()) {
         step_dir = 1 - step_dir;
@@ -102,34 +98,32 @@ void loop() {
     }
 
     if (M5.BtnB.wasPressed()) {
-    #ifndef VERSION_1_0
-        driver.resetMotor(0,0);
-        driver.resetMotor(1,0);
-        driver.resetMotor(2,0);
+#ifndef VERSION_1_0
+        driver.resetMotor(0, 0);
+        driver.resetMotor(1, 0);
+        driver.resetMotor(2, 0);
         reset_mtr = 0;
-    #endif
+#endif
     }
 
     if (M5.BtnC.wasPressed()) {
-    #ifndef VERSION_1_0
-        if (reset_mtr == 0)
-        {
-            driver.resetMotor(0,1);
-            driver.resetMotor(1,1);
-            driver.resetMotor(2,1);
+#ifndef VERSION_1_0
+        if (reset_mtr == 0) {
+            driver.resetMotor(0, 1);
+            driver.resetMotor(1, 1);
+            driver.resetMotor(2, 1);
         }
-        driver.resetMotor(int(reset_mtr/2), (reset_mtr & 0x01));
+        driver.resetMotor(int(reset_mtr / 2), (reset_mtr & 0x01));
         reset_mtr += 1;
-        if (reset_mtr == 6)
-            reset_mtr = 0;
-    #else
+        if (reset_mtr == 6) reset_mtr = 0;
+#else
         if (micro_res == DIRECT_STEPMOTOR::kMicrosteps16) {
             micro_res = DIRECT_STEPMOTOR::kMicrosteps8;
         } else {
             micro_res = DIRECT_STEPMOTOR::kMicrosteps16;
         }
         driver.setMicrostepResolution(micro_res);
-    #endif
+#endif
     }
 
     driver.getExtIOStatus();
